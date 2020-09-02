1d9c9e693ba9a524afd5b8c826e18eaf75dc0dc9
Opilane TTHK <opilane@tthk.ee>
2020-09-02 11:39:45 +0300

    Initialize repository

---
+++ Kald-BookListProject/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kald_BookListProject.Model
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
